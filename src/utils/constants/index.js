b2ca45df7f6343345d18dcda4063adb05e7c28f3
Frankie Lam <frankie191193@gmail.com>
2022-06-25 10:53:38 +0100

    update HiddenGems URL in constants

--- src/utils/constants/index.js
+++ src/utils/constants/index.js
@@ -4,7 +4,7 @@ export const BookmarkURL = "https://frankie-bookmark.netlify.app/";
 export const SunnysideURL = "https://frankie-sunnyside.netlify.app/";
 export const ManageURL = "https://frankie-manage.netlify.app/";
 export const ZestyBurgersURL = "https://zestyburgers.netlify.app/";
-export const HiddenGemsURL = "https://next-hidden-gems.vercel.app/";
+export const HiddenGemsURL = "https://hiddengems.vercel.app/";
 
 // GitHub URLs
 export const HiddenGemsGitHubURL =
